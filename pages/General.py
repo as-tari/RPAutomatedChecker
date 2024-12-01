af05708b2fd3e2b7f19c91fb74b3d675d948d4a0
as-tari <astari915@gmail.com>
2024-12-01 22:34:43 +0700

    Update General.py

--- pages/General.py
+++ pages/General.py
@@ -1,3 +1,11 @@
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
+
 def instructions_page():
     st.title("Operating Instructions")
     st.subheader("1. Upload Student Data (Unggah Data Mahasiswa)")
