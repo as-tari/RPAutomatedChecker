b12c32d87fdf912096ef13c52c7f90176fb0dea1
as-tari <astari915@gmail.com>
2024-12-02 09:45:00 +0700

    Update test.py

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def login():
     email = st.sidebar.text_input("Email", key="login_email")  # Use a unique key for text input
     password = st.sidebar.text_input("Password", type='password', key="login_password")  # Use a unique key for password input
 
-    if st.sidebar.button("Login,:red"*"):
+    if st.sidebar.button("Login :red[*]"):
         if email == "rp.fpuaj@gmail.com" and check_hashes(password, make_hashes("rp.fpuaj@gmail.com")):
             st.session_state["logged_in"] = True
             st.success("Logged in successfully!")  # Show success message
