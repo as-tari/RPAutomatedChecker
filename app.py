c4adaacf08a7ef571ca5ebbed526af2d51314a08
as-tari <astari915@gmail.com>
2024-12-02 12:07:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,11 +74,11 @@ def show_protected_content():
         container = st.container(border=True)
         container.write(Silakan unduh file ZIP dari folder 'Pengumpulan Proposal Skripsi' terlebih dahulu. Pastikan komputer atau perangkat Anda memiliki ruang penyimpanan yang memadai.)
         st.link_button("Klik tombol ini untuk mengunduh ZIP File dari Teams", "https://studentatmajayaac.sharepoint.com/:f:/r/sites/PSL401RPGanjil2425/Shared%20Documents/Pengumpulan%20Proposal%20Skripsi?csf=1&web=1&e=oiF5Qt")
-        st.caption(“Setelah tautan dibuka, Anda akan diarahkan ke folder OneDrive Atma Jaya: PSL 401 RP Ganjil 24/25 > Documents > Pengumpulan Proposal Skripsi. Di bagian atas halaman, terdapat toolbar dengan tombol ‘Download’. Klik tombol ‘Download’ untuk menyimpan file tersebut, yang akan otomatis terunduh dalam format ZIP.”)
+        st.caption(Setelah tautan dibuka, Anda akan diarahkan ke folder OneDrive Atma Jaya: PSL 401 RP Ganjil 24/25 > Documents > Pengumpulan Proposal Skripsi. Di bagian atas halaman, terdapat toolbar dengan tombol ‘Download’. Klik tombol ‘Download’ untuk menyimpan file tersebut, yang akan otomatis terunduh dalam format ZIP.)
 
         # Upload Data Mahasiswa RP (Excel)
         uploaded_excel = st.file_uploader("Unggah Data Mahasiswa RP"), type=["xlsx"])
-        st.caption("Unggah file dengan format Excel (.xlsx)")
+        st.caption(Unggah file dengan format Excel (.xlsx))
         students_data = {}
 
         if uploaded_excel:
