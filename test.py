664848568ec1e9c66110b0e273342812e7130297
as-tari <astari915@gmail.com>
2024-12-02 09:54:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import zipfile
 # Display the logo in the sidebar
 image = Image.open('logo.png')
 st.sidebar.image(image, width=100, output_format="PNG", clamp=True)
-st.sidebar.subheader("PSL 401 Rancangan Penelitian")
+st.sidebar.subheader("PSL 401 Rancangan Penelitian",divider="gray")
 
 st.title("📑 e-RP: RP Assistant System (Beta)")
 
