5bd32723b0583969b652239207c582927d0331eb
as-tari <astari915@gmail.com>
2024-12-02 10:07:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -39,21 +39,18 @@ def login():
     password = st.sidebar.text_input("Password :red[*]", type='password', key="login_password")  # Use a unique key for password input
 
     if st.sidebar.button("Login"):
-        # Check if both email and password are empty
-        if not email.strip() and not password.strip():
-            st.warning("Both email and password are required to log in. Please enter your credentials.")
-        elif not email.strip():
-            st.warning("Email is required to log in. Please enter your email.")
-        elif not password.strip():
-            st.warning("Password is required to log in. Please enter your password.")
-        elif check_hashes(password, make_hashes("rp.fpuaj@gmail.com")):
+        if email == "rp.fpuaj@gmail.com" and check_hashes(password, make_hashes("rp.fpuaj@gmail.com")):
             st.session_state["logged_in"] = True
             st.success("Logged in successfully!")  # Show success message
+        else:
+            st.sidebar.warning("Incorrect email or password")
+
+    if st.sidebar.button("Access"):
+        if email == "rp.fpuaj@gmail.com" and check_hashes(password, make_hashes("rp.fpuaj@gmail.com")):
+            st.session_state["logged_in"] = True
             show_protected_content()
         else:
-            st.sidebar.markdown("<span style='color:red;'>Incorrect email or password</span>", unsafe_allow_html=True)
-        else:
-            login()  # Show login UI if not logged in
+            st.warning("Login is required to access the system. Please try refreshing this page and log in again.")  # Notice for login required
 
 def show_protected_content():
     st.markdown("**Selamat datang di sistem e-RP!** Aplikasi ini dirancang untuk mempermudah pengecekan kelengkapan dokumen proposal mahasiswa.")
