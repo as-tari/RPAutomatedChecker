45987f11825c4dd722c3ba9d0ef7a2918947f962
as-tari <astari915@gmail.com>
2024-12-02 20:05:10 +0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -211,18 +211,18 @@ def show_protected_content():
                     else:
                         st.warning("Pembuatan laporan gagal. Silakan mengunggah dokumen kembali.")
 
-@st.cache_data
-def uploaded_excel(x):
-    return x**2
-
-@st.cache_data
-def uploaded_zip(x):
-    return x**3
-
-if st.button("Clear All"):
-    # Clear values from *all* all in-memory and on-disk data caches:
-    # i.e. clear values from both square and cube
-    st.cache_data.clear()
+        @st.cache_data
+        def uploaded_excel(x):
+            return x**2
+        
+        @st.cache_data
+        def uploaded_zip(x):
+            return x**3
+        
+        if st.button("Clear All"):
+            # Clear values from *all* all in-memory and on-disk data caches:
+            # i.e. clear values from both square and cube
+            st.cache_data.clear()
 
     
     with tab2:
