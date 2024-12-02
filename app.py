55198e7aa0b103144d054a42e745e2e9017552ea
as-tari <astari915@gmail.com>
2024-12-02 12:15:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,8 @@ def show_protected_content():
 
     with tab2:
         st.subheader("Cek Kelengkapan", divider="gray")
-
-        container = st.container(border=True)
-        container.write("Silakan unduh file ZIP dari folder 'Pengumpulan Proposal Skripsi' terlebih dahulu. Pastikan komputer atau perangkat Anda memiliki ruang penyimpanan yang memadai.")
-        st.link_button("Klik tombol ini untuk mengunduh ZIP File dari Teams", "https://studentatmajayaac.sharepoint.com/:f:/r/sites/PSL401RPGanjil2425/Shared%20Documents/Pengumpulan%20Proposal%20Skripsi?csf=1&web=1&e=oiF5Qt")
+        st.caption("Silakan unduh file ZIP dari folder 'Pengumpulan Proposal Skripsi' terlebih dahulu. Pastikan komputer atau perangkat Anda memiliki ruang penyimpanan yang memadai.")
+        st.link_button("Klik tombol ini untuk mengunduh ZIP File dari Teams :red[*]", "https://studentatmajayaac.sharepoint.com/:f:/r/sites/PSL401RPGanjil2425/Shared%20Documents/Pengumpulan%20Proposal%20Skripsi?csf=1&web=1&e=oiF5Qt")
         st.caption("Setelah tautan dibuka, Anda akan diarahkan ke folder OneDrive Atma Jaya: PSL 401 RP Ganjil 24/25 > Documents > Pengumpulan Proposal Skripsi. Di bagian atas halaman, terdapat toolbar dengan tombol _Download_. Klik tombol _Download_ untuk menyimpan file tersebut, yang akan otomatis terunduh dalam format ZIP.")
 
         # Upload Data Mahasiswa RP (Excel)
